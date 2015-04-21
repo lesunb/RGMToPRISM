488aa5bcc2d851c405dcd54790ac23cd9204bba3
Genaina <genainarodrigues@gmail.com>
2015-04-21 16:44:04 -0300

    removendo annotation de overwrite

--- src/main/java/br/unb/cic/rtgoretoprism/action/PRISMCodeGenerationAction.java
+++ src/main/java/br/unb/cic/rtgoretoprism/action/PRISMCodeGenerationAction.java
@@ -119,14 +119,11 @@ public class PRISMCodeGenerationAction extends AbstractCodeGeneractionAction {
 		}
 	}
 	
-
-	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void init(IWorkbenchWindow window) {
 		this.window = window;		
 	}
